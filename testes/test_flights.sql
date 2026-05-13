be38e59d81434f098e7245df1f1ec10545e14ac2
Thomas Todt <tbtodt@inf.ufpr.br>
2026-05-13 11:39:07 -0300

    preparar pra testar

--- testes/test_flights.sql
+++ testes/test_flights.sql
@@ -1,6 +1,8 @@
+.timer on
+
 -- INSTALL '/home/thomas/limaduck/mockdb/build/release/extension/duckteste/duckteste.duckdb_extension';
 LOAD 'duckteste';
 
-CREATE TABLE flights AS SELECT * FROM read_csv_auto('/home/thomas/limaduck/DCValidity/datasets/flights.csv');
+CREATE TABLE flights_20 AS SELECT * FROM read_csv_auto('/home/thomas/mestrado/DCValidity/datasets/flights.csv');
 
-SELECT * FROM lima_discover('flights');
+SELECT * FROM lima_discover('flights_20', num_lines=10000, threshold=0.00000001);
